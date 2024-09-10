a18953b673790fbed0ef808cd357393152bcb491
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2024-09-10 10:16:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import logo from "/logo.png";
 import "./App.css";
 import Clock from "./Clock";
 
 function App() {
   return (
     <>
-      <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
-      </div>
       <h1>World Clock</h1>
       <div className="card">
         <Clock timeZone="Asia/Singapore" />
